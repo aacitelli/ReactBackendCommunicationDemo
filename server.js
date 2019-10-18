ae55e3fb129187c53e9df5d88d1e7bd4f0cf4ddb
Anden Acitelli <a.acitelli@gmail.com>
2019-10-17 23:50:02 -0400

    I've honestly spent the past two hours troubleshooting this and it still isn't working

---
+++ server.js
@@ -0,0 +1,22 @@
+// NPM Imports
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+// Setting Up Express's Server
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Handle post request to "/" route 
+app.post("/", (req, res) => 
+{ 
+    console.log("Backend recieved this: ");
+    console.log(req.body);
+})
+
+// Start up server at given port 
+app.listen(5000, () => 
+{
+    console.log("Server listening on port 5000!");
+})
--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component 
+{
+  // Fires when the user clicks the button
+  // Grabs the data and sends it via HTTP "POST" request to Backend server using hardcoded port 
+  sendDataToBackend()
+  {
+    let inputContents = document.getElementById("userInputField").value;
+    console.log("Sending the following data: " + inputContents);
+
+    fetch("http://localhost:5000/", {
+      method: "post",
+      headers: {
+        "content-type": "text/plain"
+      },
+      body: inputContents
+    });
+  }
+
+  // Returns the HTML (technically, JSX) that the component renders 
+  render()
+  {
+    return (
+      <div className="App">
+        <input type="text" id="userInputField" placeholder="Input Text Here..."></input>
+        <button id="sendNumberToBackend" onClick={this.sendDataToBackend}>Send Number To Backend</button>
+      </div>
+    );
+  }  
 }
-
-export default App;
